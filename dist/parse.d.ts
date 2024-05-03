15d330157e640ae02fb969e110c814aec83f678d
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-03 21:44:23 +0200

    Build files

--- dist/parse.d.ts
+++ dist/parse.d.ts
@@ -1,4 +1,4 @@
-import { AxiosRequestConfig } from 'axios';
+import { Options as AxiosRequestConfig } from 'redaxios';
 declare const _default: (url: string, config?: AxiosRequestConfig) => Promise<{
     title: any;
     description: any;
